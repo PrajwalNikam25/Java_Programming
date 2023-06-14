65ddee87e638ba37357b1acd09e4be5fc13857d8
PrajwalNikam <nprajwal342@gmail.com>
2023-06-14 20:22:42 +0530

    For loop JAVA

---
+++ for loop/program1.java
@@ -0,0 +1,8 @@
+	class demo{
+		public static void main(String[] a){
+			int N =5;
+			for(int i=1; i<=5; i++){
+				System.out.println(i);
+			}
+		}
+	}
---
+++ for loop/program2.java
@@ -0,0 +1,10 @@
+	class demo{
+		public static void main(String[] s){
+			int N = 6;
+			for(int i=1;i<=6;i++){
+			if(i%2==1){
+				System.out.println(i);
+			}
+			}
+		}
+	}
