5b953e53912689ef63f7c10405ab6ddf398caf48
PrajwalNikam <nprajwal342@gmail.com>
2023-09-11 23:41:58 +0530

    FileHandling

---
+++ FileHandling1/FileDemo2.java
@@ -0,0 +1,17 @@
+// FileHandling Dmeo
+
+import java.io.*;
+
+class Demo{
+
+	public static void main(String[] a){
+	
+		File f = new File("Core2Web2.txt");
+
+		f.mkdir();
+	}
+}
+
+// This code For Create New Folder:
+// It Does Not Throw IOException:
+
