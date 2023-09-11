75e4958f5026252910d29c870bd5561113f7a557
PrajwalNikam <nprajwal342@gmail.com>
2023-09-11 23:35:51 +0530

    FileHandling

---
+++ FileHandling/FileDemo.java
@@ -0,0 +1,13 @@
+// File Handling Demo:
+
+import java.io.*;
+
+class FileDemo{
+
+	public static void main(String[] a)throws IOException{
+	
+		File f = new File("Core2Web.txt");
+
+		f.createNewFile();
+	}
+}
